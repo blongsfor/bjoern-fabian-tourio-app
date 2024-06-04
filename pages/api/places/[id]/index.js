d102e8d5a30beb303753e8bb9c3566c208efea29
Björn <bjoern.n@mailbox.org>
2024-06-04 19:49:39 +0200

    add delete button for comments

--- pages/api/places/[id]/index.js
+++ pages/api/places/[id]/index.js
@@ -24,10 +24,32 @@ export default async function handler(request, response) {
 
     response.status(200).json(placeToUpdate);
   }
+  // if (request.method === "DELETE") {
+  //   await Place.findByIdAndDelete(id);
+  //   response.status(200).json({ status: "place successfully deleted" });
+  // }
+
   if (request.method === "DELETE") {
-    await Place.findByIdAndDelete(id);
-    response.status(200).json({ status: "place successfully deleted" });
+    const { commentId } = request.body;
+
+    if (commentId) {
+      // Deleting a specific comment from the place
+      await Comment.findByIdAndDelete(commentId);
+      await Place.findByIdAndUpdate(id, {
+        $pull: { comments: commentId },
+      });
+      return response
+        .status(200)
+        .json({ status: "comment successfully deleted" });
+    } else {
+      // Deleting the place
+      await Place.findByIdAndDelete(id);
+      return response
+        .status(200)
+        .json({ status: "place successfully deleted" });
+    }
   }
+
   if (request.method === "POST") {
     const commentData = request.body;
     const createdComment = await Comment.create(commentData);
