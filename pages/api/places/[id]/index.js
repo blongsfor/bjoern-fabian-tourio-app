0e516ac50804d233d99c3660cfeb6b4e22ab8808
Björn <bjoern.n@mailbox.org>
2024-06-04 10:42:29 +0200

    step one done - connect to db

--- pages/api/places/[id]/index.js
+++ pages/api/places/[id]/index.js
@@ -1,23 +1,12 @@
-import { db_places } from "../../../../lib/db_places";
-import { db_comments } from "../../../../lib/db_comments";
+import { dbConnect } from "../../../../lib/dbConnect";
+import Place from "../../../../lib/models/place.js";
 
-export default function handler(request, response) {
+export default async function handler(request, response) {
   const { id } = request.query;
+  await dbConnect();
 
-  if (!id) {
-    return;
+  if (request.method === "GET") {
+    const place = await Place.findById(id);
+    return response.status(200).json(place);
   }
-
-  const place = db_places.find((place) => place._id.$oid === id);
-  const comment = place?.comments;
-  const allCommentIds = comment?.map((comment) => comment.$oid) || [];
-  const comments = db_comments.filter((comment) =>
-    allCommentIds.includes(comment._id.$oid)
-  );
-
-  if (!place) {
-    return response.status(404).json({ status: "Not found" });
-  }
-
-  response.status(200).json({ place: place, comments: comments });
 }
--- pages/api/places/index.js
+++ pages/api/places/index.js
@@ -1,5 +1,15 @@
-import { db_places } from "../../../lib/db_places";
+import dbConnect from "../../../lib/dbConnect.js";
+import Place from "../../../lib/models/place.js";
 
-export default function handler(request, response) {
-  return response.status(200).json(db_places);
+export default async function handler(request, response) {
+  await dbConnect();
+  if (request.method === "GET") {
+    const places = await Place.find();
+
+    if (!places) {
+      return response.status(404).json({ status: "No places found" });
+    }
+
+    return response.status(200).json(places);
+  }
 }
